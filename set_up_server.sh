bc81a955805802abd5d3a121b0ccc7d51f546945
Ubuntu <ubuntu@ip-172-31-85-72.ec2.internal>
2021-05-04 09:19:18 +0000

    add input

--- set_up_server.sh
+++ set_up_server.sh
@@ -5,12 +5,10 @@ echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 jupyter notebook --generate-config
 
-$HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
-read -r -d '' CONFIG << EOF
-conf = get_config()
+HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
+CONFIG="conf = get_config()
 conf.NotebookApp.ip = '0.0.0.0'
 conf.NotebookApp.password = u'$HASH'
-conf.NotebookApp.port = 9999
-EOF
-echo "$CONFIG" >> ~/.jupyter/jupyter_notebook_config.py_
+conf.NotebookApp.port = 9999"
+echo "$CONFIG" > ~/.jupyter/jupyter_notebook_config.py_
 echo "$CONFIG"
