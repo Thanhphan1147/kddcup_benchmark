d3cf988273b01f536aad7a579fd7615bbe9244f4
Ubuntu <ubuntu@ip-172-31-55-215.ec2.internal>
2021-05-07 08:22:12 +0000

    v2

--- set_up_server.sh
+++ set_up_server.sh
@@ -2,13 +2,11 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash Anaconda3-2019.03-Linux-x86_64.sh
 echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
-jupyter notebook --generate-config
 
 HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
 CONFIG="conf = get_config()
 conf.NotebookApp.ip = '0.0.0.0'
 conf.NotebookApp.password = u'$HASH'
 conf.NotebookApp.port = 9999"
-echo "$CONFIG" > ~/.jupyter/jupyter_notebook_config.py
+echo "$CONFIG" > ~/config.py
 echo "$CONFIG"
