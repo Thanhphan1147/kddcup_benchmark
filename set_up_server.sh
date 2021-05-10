54d16bc0b73d673d520e9cbc00665aba468166eb
tphan025 <phantrungthanh25@gmail.com>
2021-05-10 11:20:26 +0200

    cross-validation

--- set_up_server.sh
+++ set_up_server.sh
@@ -1,8 +1,9 @@
-
-wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-bash Anaconda3-2019.03-Linux-x86_64.sh
+#!/bin/bash
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+bash Anaconda3-2020.11-Linux-x86_64.sh
 echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
-
+PATH="/home/ubuntu/anaconda3/bin:$PATH"
+echo $PATH
 HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
 CONFIG="conf = get_config()
 conf.NotebookApp.ip = '0.0.0.0'
