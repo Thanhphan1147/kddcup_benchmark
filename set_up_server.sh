22ac2aa402e153611a6311c51106f0c4bd06bb18
tphan <phantrungthanh25@gmail.com>
2021-05-13 09:56:27 +0000

    DO script

--- set_up_server.sh
+++ set_up_server.sh
@@ -2,7 +2,8 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 bash Anaconda3-2020.11-Linux-x86_64.sh
 echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
-PATH="/home/ubuntu/anaconda3/bin:$PATH"
+# PATH="/home/ubuntu/anaconda3/bin:$PATH"
+PATH="/root/anaconda3/bin:$PATH"
 echo $PATH
 HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
 CONFIG="conf = get_config()
