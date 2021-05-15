e2ea5ea02d5a39d208a8074375d896bcdf45695e
Ubuntu <ubuntu@ip-172-31-70-12.ec2.internal>
2021-05-15 17:19:06 +0000

    update

--- launch_notebook.sh
+++ launch_notebook.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+source ~/.bashrc
 r=$(which python)
 echo "python: $r"
 useradd jupyter
 # su jupyter
 jupyter notebook --generate-config
-cat /tmp/config.py >> ~/.jupyter/jupyter_notebook_config.py
-jupyter notebook --allow-root
+cat config > ~/.jupyter/jupyter_notebook_config.py
+jupyter notebook
--- set_up_server.sh
+++ set_up_server.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
-bash Anaconda3-2020.11-Linux-x86_64.sh
-echo "export PATH=/root/anaconda3/bin:$PATH" >> ~/.bashrc
+# wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+# bash Anaconda3-2020.11-Linux-x86_64.sh
 # echo "export PATH="/home/ubuntu/anaconda3/bin:$PATH" >> ~/.bashrc
-# PATH="/home/ubuntu/anaconda3/bin:$PATH"
-PATH="/root/anaconda3/bin:$PATH"
 echo $PATH
 
 HASH="sha1:b07c86db1a39:696471a7f56df7014738a7da317d83442a010da8"
-CONFIG="conf = get_config()
-conf.NotebookApp.ip = '0.0.0.0'
-conf.NotebookApp.password = u'$HASH'
-conf.NotebookApp.port = 9999"
+CONFIGFILE="config"
+echo 'config: $CONFIGFILE'
 
-echo "$CONFIG" > /tmp/config.py
-echo "$CONFIG"
+echo 'conf = get_config()' > config
+echo "conf.NotebookApp.ip = '0.0.0.0'" >> config
+echo "conf.NotebookApp.password = u'$HASH'" >> config
+echo "conf.NotebookApp.port = 9999" >> config
+
+cat config
+echo "$CONFIGFILE"
+
+./launch_notebook.sh
 
 # wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
 # wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.testdata.unlabeled.gz
