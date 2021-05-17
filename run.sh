ea858999c69473685c166e85990f03832e4c8ded
tphan025 <phantrungthanh25@gmail.com>
2021-05-17 10:01:38 +0200

    update

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ useradd -m jupyter
 cd /home/jupyter
 echo "$(pwd), $(whoami)"
 
-su -c "git clone https://github.com/Thanhphan1147/kddcup_benchmark.git && cd kddcup_benchmark" jupyter
+su -c "git clone https://github.com/Thanhphan1147/kddcup_benchmark.git" jupyter
+cd kddcup_benchmark
 su -c "scripts/get_script.sh" jupyter
